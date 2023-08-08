e3015066fb2f3977eb624a857fe16c9b9f044f8e
grzesiek <grzesiekkedzior@gmail.com>
2023-08-08 11:31:04 +0200

    Add TabWidgets
    Change the structure of the project to TabWidgets and do simple refactoring.

--- AudioLink.pro
+++ AudioLink.pro
@@ -27,3 +27,5 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 
 RESOURCES += \
     res.qrc
+
+DISTFILES +=
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowIcon(QIcon(":/images/img/radio30.png"));
     w.show();
     return a.exec();
 }
