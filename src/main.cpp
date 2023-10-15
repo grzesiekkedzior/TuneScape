8707abaa632633780cbf6bd1ff5c6642e7db806a
grzesiek <grzesiekkedzior@gmail.com>
2023-10-15 18:02:12 +0200

    Simple refactoring.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowIcon(QIcon(":/images/img/radio30.png"));
+    w.setWindowTitle("AudioLink");
     w.show();
     return a.exec();
 }
