aa48a6dcfb3eb3986d0eced2c586c65af2d7b77e
grzesiek <grzesiekkedzior@gmail.com>
2024-01-27 23:45:48 +0100

    Add dark mode, first release. Beta

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -104,7 +104,7 @@ void MainWindow::infoDialogApp()
 
     aboutTuneScape = new QLabel;
     aboutTuneScape->setText(
-        "<p style=\"font-size: 18pt; line-height: 1.5;\">TuneScape 2.0.1</p>"
+        "<p style=\"font-size: 18pt; line-height: 1.5;\">TuneScape 2.1.0</p>"
         "This is a free and Open Source online radio player based on "
         "<a href=\"https://www.radio-browser.info\">radio-browser service </a> "
         "and <a href=\"https://www.icecast.org/\">Ice-Cast service.</a>"
