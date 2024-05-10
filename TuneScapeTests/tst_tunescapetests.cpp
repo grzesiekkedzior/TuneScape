5ec60ad753bf25472b83c1acd014b9b268f66edc
grzesiek <grzesiekkedzior@gmail.com>
2024-05-10 10:05:39 +0200

    Create a new structure for project-adding tests and the possibility to add a new subproject.

---
+++ TuneScapeTests/tst_tunescapetests.cpp
@@ -0,0 +1,32 @@
+#include <QCoreApplication>
+#include <QtTest>
+
+// add necessary includes here
+
+class TuneScapeTests : public QObject
+{
+    Q_OBJECT
+
+public:
+    TuneScapeTests();
+    ~TuneScapeTests();
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void test_case1();
+};
+
+TuneScapeTests::TuneScapeTests() {}
+
+TuneScapeTests::~TuneScapeTests() {}
+
+void TuneScapeTests::initTestCase() {}
+
+void TuneScapeTests::cleanupTestCase() {}
+
+void TuneScapeTests::test_case1() {}
+
+QTEST_MAIN(TuneScapeTests)
+
+#include "tst_tunescapetests.moc"
