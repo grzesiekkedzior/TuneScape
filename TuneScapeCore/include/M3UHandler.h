9344603537362bd32e28f03df8b41f4f62f58fd6
grzesiek <grzesiekkedzior@gmail.com>
2025-04-23 12:40:29 +0200

    Add the interface to implement.

--- TuneScapeCore/include/M3UHandler.h
+++ TuneScapeCore/include/M3UHandler.h
@@ -1,11 +1,37 @@
 #ifndef M3UHANDLER_H
 #define M3UHANDLER_H
 #include <QObject>
+#include <QList>
+#include <QString>
 
 class M3UHandler : public QObject {
     Q_OBJECT
 public:
-    M3UHandler();
+    explicit M3UHandler(QObject *parent = nullptr);
+
+    struct TableRow
+    {
+        QString station;
+        QString genre;
+        QString country;
+        QString stationUrl;
+    };
+
+private:
+    //Load pls
+    QList<TableRow> importM3Ufile(const QString path);
+    bool isM3Ufile();
+    bool isRadioInPlaylist(const QString data, const QString playlist);
+    bool saveOnTuneScapeFile();
+
+    //Export pls
+    bool exportM3Ufile();
+    bool convertToM3Ufile();
+
+    //Common
+    static bool fileExists(const QString path);
+
+
 };
 
 #endif // M3UHANDLER_H
--- TuneScapeCore/src/M3UHandler.cpp
+++ TuneScapeCore/src/M3UHandler.cpp
@@ -1,6 +1,41 @@
 #include "include/M3UHandler.h"
 
-M3UHandler::M3UHandler()
+M3UHandler::M3UHandler(QObject *parent)
+{
+
+}
+
+bool M3UHandler::isM3Ufile()
+{
+
+}
+
+bool M3UHandler::isRadioInPlaylist(const QString data, const QString playlist)
+{
+
+}
+
+bool M3UHandler::saveOnTuneScapeFile()
+{
+
+}
+
+bool M3UHandler::exportM3Ufile()
+{
+
+}
+
+bool M3UHandler::convertToM3Ufile()
+{
+
+}
+
+bool M3UHandler::fileExists(const QString path)
+{
+
+}
+
+QList<M3UHandler::TableRow> M3UHandler::importM3Ufile(const QString path)
 {
 
 }
