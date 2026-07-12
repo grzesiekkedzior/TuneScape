8b920c34770f69f35573202e0472b7a60b996e7b
grzesiek <grzesiekkedzior@gmail.com>
2026-07-12 17:04:05 +0200

    refactor: introduce RadioStationsModel

---
+++ TuneScapeCore/include/model/RadioStationsModel.h
@@ -0,0 +1,26 @@
+#ifndef RADIOSTATIONSMODEL_H
+#define RADIOSTATIONSMODEL_H
+#include <QAbstractTableModel>
+#include <QObject>
+#include "include/data/RadioStation.h"
+
+enum class Column { StationColumn, GenreColumn, CountryColumn, HomepageColumn, ColumnCount };
+
+class RadioStationsModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+public:
+    RadioStationsModel(QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent) const override;
+    int columnCount(const QModelIndex &parent) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+
+private:
+    QVector<RadioStation> m_stations;
+    Column column = Column::ColumnCount;
+};
+
+#endif // RADIOSTATIONSMODEL_H
